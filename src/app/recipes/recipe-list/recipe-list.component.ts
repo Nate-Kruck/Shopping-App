d17b84ec90e3cea7f908e68b64eb04ff32a278d1
Nathan Kruckenberg <natekruck1990@gmail.com>
2022-02-06 20:22:39 -0600

    recipe detail component added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   recipes : Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_960_720.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_960_720.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_960_720.jpg'),
   ]
 
   constructor() { }
