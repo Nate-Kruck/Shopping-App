7dabf17461d9b165dda145a544ff4e5942497b3f
Nathan Kruckenberg <natekruck1990@gmail.com>
2022-02-06 20:03:28 -0600

    recipe list component is taking shape

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes : Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_960_720.jpg')
+  ]
 
   constructor() { }
 
