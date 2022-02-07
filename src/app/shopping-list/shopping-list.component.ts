f967d9520221f86a5fb7e907c3b42c5388327f42
Nathan Kruckenberg <natekruck1990@gmail.com>
2022-02-07 11:42:58 -0600

    created add/delete/clear buttons

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
-  
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Oranges', 3)
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
